0c601a31e2e6cabdd4d87d678acd271c594de790
rifat1122ddf-commits <rifat1122ddf@gmail.com>
2026-05-19 16:36:58 +0600

    Update events.js

--- events.js
+++ events.js
@@ -1,1 +1,37 @@
+// events.js
+class EventBus {
+  constructor() {
+    this.events = new Map();
+  }
+  
+  on(event, callback) {
+    if (!this.events.has(event)) this.events.set(event, []);
+    this.events.get(event).push(callback);
+    return () => this.off(event, callback);
+  }
+  
+  off(event, callback) {
+    const callbacks = this.events.get(event);
+    if (callbacks) {
+      this.events.set(event, callbacks.filter(cb => cb !== callback));
+    }
+  }
+  
+  emit(event, ...args) {
+    const callbacks = this.events.get(event);
+    if (callbacks) {
+      callbacks.forEach(cb => cb(...args));
+    }
+  }
+  
+  once(event, callback) {
+    const wrapper = (...args) => {
+      callback(...args);
+      this.off(event, wrapper);
+    };
+    this.on(event, wrapper);
+  }
+}
 
+const events = new EventBus();
+module.exports = { events };
